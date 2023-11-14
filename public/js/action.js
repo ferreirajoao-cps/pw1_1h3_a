e7390c7f21e822f9dd67da965be6beaca8105e8c
Aluno <joaoferreira406@etec.sp.gov.br>
2023-11-14 20:45:10 -0300

    Aula1411 fundamentos de DOM HTML

--- public/js/action.js
+++ public/js/action.js
@@ -1,14 +1,28 @@
-function somaNumero(n1, n2, op){
-let resultado=0;
-if(op==="c"){
-    resultado=(n1+n2)
-    console.table(resultado)
-    return
+function calculaNumeros(){
+ let n1 = parseInt (document.getElementById("usuario").value)
+ let n2 =parseInt (document.getElementById("senha").value)
+ let op =document.getElementById("operation").value
+ let result
+ if(op === "+"){
+    result=(n1+n2)
+    alert("Soma: "+n1+" "+op+" "+n2+ "=" +result)
+ }
+ else if(op === "-"){
+    result=(n1-n2)
+    alert("Subtração: "+n1+" "+op+" "+n2+ "=" +result)
+ }
+ else if(op === "*"){
+    result=(n1*n2)
+    alert("Multiplicação: "+n1+" "+op+" "+n2+ "=" +result)
 }
+else if(op === "/"){
+    result=(n1/n2)
+    alert("Divisão: "+n1+" "+op+" "+n2+ "=" +result)
+}
+else if ( op=== "sqrt"){
+    result= Math.sqrt(n1+n2)
+    alert("Raiz: "+result)
+}
+else { alert("Operação inválida") }
 
-if(op==="a"){
-    resultado=(n1-n2)
-    alert(resultado)
-    return
-}
 }
