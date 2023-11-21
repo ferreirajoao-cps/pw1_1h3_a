3488b3eb4f964aa71924340794f1ab85af128da3
Aluno <joao.ferreira406@etec.sp.gov.br>
2023-11-21 20:42:40 -0300

    Aula 2111 Modulos em JavaScript

---
+++ public/js/moduloDados.js
@@ -0,0 +1,6 @@
+const userLogin = [
+    {id: 1001, username: "Pato_lokko", senha: "p4t1nh0lokk0", email: "ferreirajoaogabriel14@gmail.com"},
+    {id:1002, username: "Frozen_Soldier", senha: "AmigaodoPeito70%", email: "cayrorick@gmail.com"}
+]
+
+export default userLogin
