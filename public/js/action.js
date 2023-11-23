9433ca947db410a1b1922d298337d3bf46dabf43
ferreirajoao-cps <joao.ferreira406@etec.sp.gov.br>
2023-11-22 22:38:38 -0300

    Aula2211 Arrays e Interações

--- public/js/action.js
+++ public/js/action.js
@@ -1,10 +1,17 @@
 import userLogin from './moduloDados.js'
 
 const login = document.getElementById("login")
+const cadastro = document.getElementById("cadastro")
 
 login.addEventListener("click", (e)=> {
     let user = document.getElementById("usuario").value 
     let senha = document.getElementById("senha").value
-    console.table(user, senha)
-    console.table(userLogin)
+    console.table(userLogin[0])
 })
+cadastro.addEventListener("click", (e) => {
+    var usuario =userLogin.find((userLogin) => userLogin.email === "ferreirajoaogabriel14@gmail.com")
+    console.log(usuario)
+})
+function encontarUsuario(value, index, array){
+    console.table(value.email)
+}
